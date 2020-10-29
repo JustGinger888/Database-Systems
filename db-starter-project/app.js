0d6349ad78b9f4f972371a31dc333050ae715e2e
JustGinger888 <ethanmoffatt888@icloud.com>
2020-10-29 14:17:38 +0000

    task 3

--- db-starter-project/app.js
+++ db-starter-project/app.js
@@ -1,14 +1,25 @@
 require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const app = express();
+const mongoose = require("mongoose");
+
+const { WEB_PORT, MONGODB_URI } = process.env;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+  console.log(
+    "MongoDB connection error. Please make sure MongoDB is running.",
+  );
+  process.exit();
+});
+
+const tasterController = require("./controllers/taster");
+app.get("/tasters", tasterController.list);
+app.get("/tasters/delete/:id", tasterController.delete);
+
 app.set("view engine", "ejs");
-
-/**
- * notice above we are using dotenv. We can now pull the values from our environment
- */
-
-const { WEB_PORT } = process.env;
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -23,3 +34,8 @@ app.get("/tasters", (req, res) => {
 app.listen(WEB_PORT, () => {
   console.log(`Example app listening at http://localhost:${WEB_PORT}`);
 });
+
+
+
+
+
